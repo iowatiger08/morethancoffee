2eb516a293c5ff68da898616c70ae5233e1b8dfc
Tony Dillon Hansen <iowatiger08@gmail.com>
2023-02-08 22:05:42 -0600

    updates for images

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '../constants.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  HOME = Constants.HOME_LABEL;
 
   constructor() { }
 
